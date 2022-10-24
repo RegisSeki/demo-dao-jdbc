22e8a461e45594bc73ccdc8623b11ec8c733607c
RegisSeki <regisseki@gmail.com>
2022-10-24 09:48:13 -0300

    Create Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department d = new Department(1, "Wears");
-		System.out.println(d);
+		Department department = new Department(1, "Wears");
+		
+		LocalDate date = LocalDate.parse("1990-07-12");
+		Seller seller = new Seller(21,"Bob", "bob@email.com", date, 3000.00, department); 
+		
+		System.out.println(department);
+		System.out.println(seller);
 	}
 
 }
